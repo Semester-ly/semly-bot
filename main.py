ca121bee516c06e46b7f935b93e878889f8e305b
root <root@DESKTOP-UKV0O21>
2024-09-23 23:50:47 -0400

    reverting

--- main.py
+++ main.py
@@ -1,17 +1,16 @@
 from reminder import ReminderCog
-# from anonymessage import AnonyMessageCog
-# from anonypoll import AnonyPollCog
-# from execute import ExecuteCog
+from anonymessage import AnonyMessageCog
+from anonypoll import AnonyPollCog
+from execute import ExecuteCog
 from nextcord import Intents
 from nextcord.ext import commands
 from decouple import config
 
-# ints = Intents.default()
-# ints.members = True
-# bot = commands.Bot(command_prefix="$", intents=ints)
-bot = commands.Bot(command_prefix="$")
+ints = Intents.default()
+ints.members = True
+bot = commands.Bot(command_prefix="$", intents=ints)
 bot.add_cog(ReminderCog(bot))
-# bot.add_cog(AnonyMessageCog(bot))
-# bot.add_cog(AnonyPollCog(bot))
-# bot.add_cog(ExecuteCog(bot))
-bot.run(config('TOKEN'))
+bot.add_cog(AnonyMessageCog(bot))
+bot.add_cog(AnonyPollCog(bot))
+bot.add_cog(ExecuteCog(bot))
+bot.run(config("TOKEN"))
